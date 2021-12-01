eb2e993a81a0eb1e8deaa7c79b1db5dd0f3e603a
Дмитрий Минаков <m1nak0vd@yandex.ru>
2021-12-01 22:24:42 +0300

    Добавьте файлы проекта.

---
+++ Lab_7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            CalkSum(a, b);
+            Console.ReadKey();
+        }
+        static void CalkSum(int a, int b)
+        {
+            int Sum = a + b;
+            Console.WriteLine(Sum);
+        }
+        static void CalkSum(int a, int b, int c)
+        {
+            int Sum = a + b + c;
+            Console.WriteLine(Sum);
+        }
+    }
+}
---
+++ Ref/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ref
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            CalkSum(ref a, b);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+        static void CalkSum(ref int a, int b)
+        {
+            a = +b;
+        }
+    }
+}
+}
